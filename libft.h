d0fd5bd95ccb165e649b4ee396dc2025e8cf3052
Gabriele katana <gabriele.scuderi.00@gmail.com>
2024-01-20 01:34:16 +0100

    writing libft w/ ft declaraion

--- libft.h
+++ libft.h
@@ -1,18 +1,36 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: gscuderi <gscuderi@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/08 13:24:22 by gscuderi          #+#    #+#             */
-/*   Updated: 2024/01/15 18:55:40 by gscuderi         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
-#define LIBFT_H
+#ifndef LIBFT_H
+# define LIBFT_H
 
-#include <stddef.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
 
-//prototipo delle funzioni
+
+int			ft_atoi(const char *ptr);
+void		ft_bzero(void *ptr, size_t n);
+
+int			ft_isalnum(int c);
+int			ft_isalpha(int c);
+int			ft_isascii(int c);
+int			ft_isdigit(int c);
+int			ft_isprint(int c);
+
+void		*ft_memchr(const void *s, int c, size_t n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
+void		*ft_memcpy(void *dst, const void *src, size_t n);
+void		*ft_memmove(void *dst, const void *src, size_t n);
+void		ft_memset(void *b, int c, size_t len);			
+
+char		*ft_strchr(const char *s, int c);
+size_t		ft_strlcat(char *dest, const char *src, size_t dstsize);
+size_t		ft_strlen(const char *s);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char		*ft_strrchr(const char *s, int c);
+
+int			ft_tolower(int c);
+int			ft_toupper(int c);
+
+
 #endif
